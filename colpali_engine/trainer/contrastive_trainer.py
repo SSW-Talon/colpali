4d7467946e311f51fc7571964eb78a05d3c55eaa
SSW-Talon <70064164+SSW-Talon@users.noreply.github.com>
2025-12-11 17:36:53 +0800

    Update contrastive_trainer.py

--- colpali_engine/trainer/contrastive_trainer.py
+++ colpali_engine/trainer/contrastive_trainer.py
@@ -116,7 +116,7 @@ class ContrastiveTrainer(Trainer):
 
         return self.accelerator.prepare(dataloader)
 
-    def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
+    def _get_train_sampler(self, dataset=None) -> Optional[torch.utils.data.Sampler]:
         if self.train_dataset_list is None:
             return super()._get_train_sampler()
 
